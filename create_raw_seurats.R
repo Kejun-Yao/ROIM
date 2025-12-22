b70522bd1603a89500bb314817e54351e52edfb7
andrei-stoica26 <andreistoica@foxmail.com>
2025-12-22 14:32:15 +0800

    Added functions for creating raw Seurats

---
+++ create_raw_seurats.R
@@ -0,0 +1,11 @@
+annotation <- GetGRangesFromEnsDb(ensdb = EnsDb.Hsapiens.v86)
+seqlevels(annotation) <- paste0('chr', seqlevels(annotation))
+
+folders <- paste0('sc-', c('0', '12', '24', 'ctr'))
+idents <- c('0h', '12h', '24h', 'Control')
+filePath <- paste0('/home/Andrei/ROInjuryData/Results/', folders, '/outs')
+seurats <- mapply(function(folder, ident) 
+    jointReadSeurats(folder, '../ROInjuryData/Results/', ident, annotation), 
+    folders, 
+    idents)
+
---
+++ load_packages.R
@@ -0,0 +1,4 @@
+library(Seurat)
+library(Signac)
+library(EnsDb.Hsapiens.v86)
+library(qs2)
